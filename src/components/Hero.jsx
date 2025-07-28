d5b3cfcd56f8c7ee27000233ac12d5f2377a8f2a
Kennedy <kenrichshiku@gmail.com>
2025-07-29 00:23:48 +0300

    Added Scroll animation to Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,12 +5,12 @@ const HeroSection = () => {
     <section className="hero-section">
       <div className="section-content">
         <div className="hero-details">
-          <h2 className="title">Best Polysite</h2>
-          <h3 className="subtitle">Premium Bottle Manufacturing!</h3>
-          <p className="description">
+          <h2 className="title" data-aos="fade-up">Best Polysite</h2>
+          <h3 className="subtitle" data-aos="fade-up" data-aos-delay="300">Premium Bottle Manufacturing!</h3>
+          <p className="description" data-aos="fade-up" data-aos-delay="400">
             Specializing in PET, PP and PC bottles for diverse industries.
           </p>
-          <div className="button">
+          <div className="button" data-aos="fade-up" data-aos-delay="500">
             <a href="#" className="button order-now">Order Now</a>
             <a href="#contact" className="button contact-us">Contact Us</a>
           </div>
