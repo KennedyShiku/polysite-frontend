5f533524468ea841398c1a5e60a89ba1117e896c
Kennedy <kenrichshiku@gmail.com>
2025-07-28 17:11:28 +0300

    Added NavBar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,20 @@
+import './index.css';
+import Navbar from './components/Navbar';
+import HeroSection from './components/HeroSection';
+import AboutSection from './components/AboutSection';
+// More components to come...
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <main>
+        <HeroSection />
+        <AboutSection />
+        {/* Add Products, Testimonials, Gallery, Contact, Footer later */}
+      </main>
+    </>
+  );
+}
+
+export default App;
