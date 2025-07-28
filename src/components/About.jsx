e6f932c7d3e2ca13482c5aa30fbbb375ac4cd841
Kennedy <kenrichshiku@gmail.com>
2025-07-29 00:13:37 +0300

    Added scroll  animation to About section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,18 +4,18 @@ const AboutSection = () => {
   return (
     <section className="about-section" id="about">
       <div className="section-content">
-        <div className="about-image-wrapper">
+        <div className="about-image-wrapper" data-aos="fade-right">
           <img src={aboutImage} alt="About" className="about-image" />
         </div>
         <div className="about-details">
-          <h2 className="section-title">About Us</h2>
-          <p className="text">
+          <h2 className="section-title" data-aos="fade-up" data-aos-delay="500">About Us</h2>
+          <p className="text" data-aos="fade-up" data-aos-delay="500">
             Polysite is a leading manufacturer of high quality plastic bottles and containers. 
             With over 15 years of experience, we serve diverse industries including pharmaceuticals, 
             beverages, chemicals and cosmetics. Our state of the art facilities ensure consistent 
             quality and timely delivery for all your packaging needs.
           </p>
-          <div className="social-link-list">
+          <div className="social-link-list" data-aos="fade-up" data-aos-delay="500">
             <a href="#" className="social-link"><i className="fa-brands fa-facebook"></i></a>
             <a href="#" className="social-link"><i className="fa-brands fa-instagram"></i></a>
             <a href="#" className="social-link"><i className="fa-brands fa-twitter"></i></a>
