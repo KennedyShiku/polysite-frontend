7a46216a1a655d95b25dcf8ffd547f9f9593b25d
Kennedy <kenrichshiku@gmail.com>
2025-07-29 12:03:59 +0300

    Added pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,34 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import 'aos/dist/aos.css';
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import './index.css'
+// import App from './App.jsx'
+// import 'aos/dist/aos.css';
+// import AOS from 'aos';
+
+// AOS.init();
+
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import { BrowserRouter } from 'react-router-dom';
+
 import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+import './index.css'; // global styles
+import './assets/style.css'; // legacy CSS
 
 AOS.init();
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
