bba66b43a3343b55febc068b201b18a54f5c6aef
Kennedy <kenrichshiku@gmail.com>
2025-07-30 16:44:19 +0300

    Cleaned up code

--- src/components/VisitCounter.jsx
+++ src/components/VisitCounter.jsx
@@ -1,35 +1,3 @@
-// import { useEffect, useState } from 'react';
-
-// const VisitorCounter = () => {
-//   const [count, setCount] = useState(0);
-
-//   useEffect(() => {
-//     const visits = localStorage.getItem('polysite-visits');
-//     const newCount = visits ? parseInt(visits) + 1 : 1;
-//     localStorage.setItem('polysite-visits', newCount);
-//     setCount(newCount);
-//   }, []);
-
-//   return (
-//     <div style={counterStyle}>
-//       👁️ Visitors: {count}
-//     </div>
-//   );
-// };
-
-// const counterStyle = {
-//   position: 'fixed',
-//   top:100,
-//   right: 20,
-//   fontSize: '0.9rem',
-//   backgroundColor: '#1E90FF',
-//   color: 'white',
-//   padding: '5px 10px',
-//   borderRadius: '8px',
-//   zIndex: 1000,
-// };
-
-// export default VisitorCounter;
 import { useEffect, useState } from 'react';
 
 const VisitorCounter = () => {
--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,3 @@
-// import { StrictMode } from 'react'
-// import { createRoot } from 'react-dom/client'
-// import './index.css'
-// import App from './App.jsx'
-// import 'aos/dist/aos.css';
-// import AOS from 'aos';
-
-// AOS.init();
-
-// createRoot(document.getElementById('root')).render(
-//   <StrictMode>
-//     <App />
-//   </StrictMode>,
-// )
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
@@ -19,9 +5,6 @@ import { BrowserRouter } from 'react-router-dom';
 
 import AOS from 'aos';
 import 'aos/dist/aos.css';
-
-// import './index.css'; // global styles
-// import './assets/style.css'; // legacy CSS
 
 AOS.init();
 
